e3b0c344317eef737280f57a41463d8f6cf0dc91
Guido Maimone <guido.maimone7@gmail.com>
2023-08-10 09:49:21 -0300

    set enviromental variables

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-port = 3000;
+require("dotenv").config();
 
 // Routes
 const mainRouter = require("./routes/api/mainRouter");
@@ -12,6 +12,6 @@ const notesRouter = require("./routes/api/notesRouter");
 app.use("/api/notes", notesRouter);
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(process.env.BACK_PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.BACK_PORT}`);
 });
