4f59c5ad1f905b91445ded312acf43f7177166c4
Guido Maimone <guido.maimone7@gmail.com>
2023-08-14 00:13:54 -0300

    updated mainController back

--- backend/src/controllers/api/mainController.js
+++ backend/src/controllers/api/mainController.js
@@ -4,26 +4,10 @@ const { Notes } = require("../../database/models");
 const apiMainController = {
   index: (req, res) => {
     const data = {
-      message: "The available endpoints are as follows:",
-      test: "http://localhost:3000/test",
-      allNotes: "GET: http://localhost:3000/api/notes",
-      oneNote: "GET: http://localhost:3000/api/notes/id",
-      createNote: "POST: http://localhost:3000/api/notes/addNote"
+      message: "The available endpoints are as follows (TBA):",
     };
     res.status(200).json(data);
-  },
-
-  test: async (req, res) => {
-    try {
-      const notes = await Notes.findAll({include: "category"});
-      res.status(200).json(notes);
-    } catch (error) {
-      console.error("Error fetching categories: ", error);
-      res.status(500).json({
-        message: "An error ocurred while fetching categories",
-      });
-    }
-  },
+  }
 };
 
 module.exports = apiMainController;
